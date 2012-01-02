f4cd307388f5a17a38a8f5670f40545c39b5961d
Zimilo <zimilo@code-trick.com>
2012-01-02 23:08:47 +0800

    up

--- config/Runtime.py
+++ config/Runtime.py
@@ -14,13 +14,13 @@ else:
     app = web.config._app
 
 if web.config.get('_session') is None:
-    session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'UserID':-1, 'UserName':'Guest', 'Status':{'LastPublishTime':0}})
+    session = web.session.Session(app, web.session.DiskStore(GLOBAL_PROJECT_ROOT + '/sessions'), initializer={'UserID':-1, 'UserName':'Guest', 'Status':{'LastPublishTime':0}})
     web.config._session = session
 else:
     session = web.config._session
 
 if web.config.get('_render') is None:
-    render = web.template.render('templates/', base='TLayout', globals={'ctx':web.config._session, 'client':web.ctx})
+    render = web.template.render(GLOBAL_PROJECT_ROOT + '/templates/', base='TLayout', globals={'ctx':web.config._session, 'client':web.ctx})
     web.config._render = render
 else:
     render = web.config._render
