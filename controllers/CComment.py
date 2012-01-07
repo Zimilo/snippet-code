dadb40197cda239a7f82f47bccfbf093335e5161
Zimilo <zimilo@code-trick.com>
2012-01-08 04:09:44 +0800

    修改评论使用短链而不是ID

--- controllers/CComment.py
+++ controllers/CComment.py
@@ -10,22 +10,21 @@ from models import MComment
 
 class CommentPost:
 	def POST(self):
-		pid = int(web.input().post_id.strip())
+		short_link = web.input().post_link.strip()
 		user_name = web.input().user_name.strip()
 		comment_content = web.input().comment_content.strip()
+
 		if not user_name or not comment_content:
 			return render.TMessage(MMessage.ConstructCommonMessage(GLOBAL_MSG_ERROR, "所有字段均不能为空", [['javascript:history.go(-1)', '返回之前的页面']]))
 		
-
-
-		post = MPost.Post.QueryDB(post_id = pid)
+		post = MPost.Post.QueryDB(short_lnk = short_link)
 
 		if not post:
 			return render.TMessage(MMessage.ConstructCommonMessage(GLOBAL_MSG_ERROR, "参数传递出错", [['javascript:history.go(-1)', '返回之前的页面']]))
 
 
 		comment = {
-			'post_id' : pid,
+			'post_id' : post.id,
 			'user_name' : user_name,
 			'comment_content' : comment_content,
 			'publish_time' : int(time.time()),
@@ -36,4 +35,4 @@ class CommentPost:
 			return render.TMessage(MMessage.ConstructCommonMessage(GLOBAL_MSG_ERROR, "提交评论失败: [" + r['ErrorMsg'] + "]。", [['javascript:history.go(-1)', '返回之前的页面']]))
 
 
-		return render.TMessage(MMessage.ConstructCommonMessage(GLOBAL_MSG_NORMAL, "发布评论成功啦!", [['/post/view/'+str(pid), '返回']]))
+		return render.TMessage(MMessage.ConstructCommonMessage(GLOBAL_MSG_NORMAL, "发布评论成功啦!", [['/' + post.link, '返回']]))
