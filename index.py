52f2bfdcbc4f4bfc3425a6064cd094ed12ee50b0
Zimilo <zimilo@code-trick.com>
2012-01-08 03:00:46 +0800

    up

--- index.py
+++ index.py
@@ -8,6 +8,6 @@ from config.Runtime import *
 if __name__ == '__main__':
 	reload(sys)
 	sys.setdefaultencoding("utf-8")
-	if DEPLOY_SERVER == "FASTCGI":
+	if DEPLOY_MODE == "FASTCGI":
 		web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
 	app.run()
