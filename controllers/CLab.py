d692028825aff5317f38671fb6cbcd24e3ea2105
Zimilo <zimilo@code-trick.com>
2012-01-08 19:55:37 +0800

    update

--- controllers/CLab.py
+++ controllers/CLab.py
@@ -9,4 +9,4 @@ from models.MMessage import MMessage
 
 class Lab:
     def GET(self):
-        return render.TMessage(MMessage.ConstructCommonMessage(GLOBAL_MSG_ERROR, "该功能正在开发中，敬请期待。", [['javascript:history.go(-1)', '返回之前的页面']]))
+        return render.TLab()
---
+++ scripts/backup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import sys
+import os
+
+
+if __name__ == "__main__":
+	print "@step1 导出所有的用户"
+	print "@step2 导出所有的代码片段"
+	print "@step3 导出所有的评论"
+	print "@step4 生成本次本分的Meta信息"
+	print "@step5 打包合并生成本次的备份文件"
